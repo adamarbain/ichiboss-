5f202ea81bc5cc55c6776432b232236dccc61635
Afiq <muhdafiq197@gmail.com>
2025-04-12 16:11:13 +0800

    trying to include ml

--- backtesting_framework/tester.py
+++ backtesting_framework/tester.py
@@ -5,13 +5,13 @@ from strategies import moving_average
 from main import VectorBacktester
 from analysis.metrics import AdvancedAnalyzer
 
-preprocessor = DataPreprocessor()
 temp = pd.read_csv('DOGE.csv')
+BTC = pd.read_csv('BTC.csv')
 
+preprocessor = DataPreprocessor()
 clean_temp = preprocessor.clean_market_data(temp)
 added_temp = preprocessor.add_returns(clean_temp)
-print(added_temp)
-
+# print(added_temp)
 
 strategy = moving_average.MovingAverageStrategy(added_temp)
 strategy.generate_signals()
@@ -22,6 +22,17 @@ backtester.run_backtest()
 results = backtester.results
 
 analysis = AdvancedAnalyzer(results)
+
+# No of trade
+# long trade
+# short trade
+# Average holding time
+# Cummulative 
+# Cummulative MDD
+# Sharp ratio
+# Success rate
+# - equity curve
+# - Heatmap analysis
 print(f"Sharp: {analysis.sharpe_ratio()}")
 print(f"MDD: {analysis.max_drawdown()}")
 print(f"Donno this one: \n{analysis.plot_waterfall()}")
